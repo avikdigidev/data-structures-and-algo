acb66f5527a52107e956604c67737ed2784e9d3d
avikdigidev <avikdigidev@gmail.com>
2021-12-29 00:41:00 +0530

    Added MaximizeSumArrays with condition i<j<k

--- src/com/arrays/MaximizeSumArrays.java
+++ src/com/arrays/MaximizeSumArrays.java
@@ -8,10 +8,12 @@ import java.util.Scanner;
 public class MaximizeSumArrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, a, b, c;
+        int n;
+        int a,b,c; //int a=2;  int b =4; int c= 6;
         System.out.println("Enter the size of desire array: ");
         n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[n]; //{1,5,2,3,4}
+
         System.out.println("Enter the first multiplier: ");
         a = sc.nextInt();
         System.out.println("Enter the second multiplier: ");
@@ -21,21 +23,33 @@ public class MaximizeSumArrays {
         for (int i = 0; i < n; i++) {
             System.out.println("Enter the " + (i + 1) + " element: ");
             arr[i] = sc.nextInt();
+            System.out.print(arr[i]+ ",");
         }
+
+
         // we need to maximize sum of (a*arr[i]+ b*arr[j] + c*arr[k]) given that i<j<k
         // first we will fix the middle term such that the array is divided into 2 parts one for i and other for k
         int[] prefixMax = getPrefixMax(arr, n, a);
         int[] suffixMax = getSuffixMax(arr, n, c);
         int ans = Integer.MIN_VALUE;
-        for (int y = 0; y <= n - 1; y++) {
-            int first = prefixMax[y];
+        /*
+         *  TO MAKE THIS PROBLEM WIH CONDITION i<=j<=k, use the below commented loop:
+         * for (int y = 0; y <= n - 1; y++) {
+         * int first = prefixMax[y];
+         * int mid = b * arr[y];
+         * int last = suffixMax[y];
+         *
+         */
+        for (int y = 1; y <= n - 2; y++) {
+
+            int first = prefixMax[y-1];
             int mid = b * arr[y];
-            int last = suffixMax[y];
+            int last = suffixMax[y+1];
             ans = Integer.max(ans, first + mid + last);
             System.out.println("First: " + first);
-            System.out.println("Second: " + mid);
-            System.out.println("Third: " + last);
-            System.out.println("Result of " + (y + 1) + " iteration" + ans);
+            System.out.println("Second: " + mid );
+            System.out.println("Third: " + last );
+            System.out.println("Result of " + (y + 1) + " iteration : " + ans);
         }
         System.out.print("Max sum possible is " + ans);
     }
