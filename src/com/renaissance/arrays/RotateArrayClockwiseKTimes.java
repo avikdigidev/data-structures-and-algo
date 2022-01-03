70fdd142638034cba9768de7eaa44f7d98d01601
avikdigidev <avikdigidev@gmail.com>
2022-01-03 11:13:31 +0530

    Updated code with correct and efficient approach

--- src/com/renaissance/arrays/RotateArrayClockwiseKTimes.java
+++ src/com/renaissance/arrays/RotateArrayClockwiseKTimes.java
@@ -2,69 +2,42 @@ package com.renaissance.arrays;
 // Java Implementation of Right Rotation
 // of an Array K number of times
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class RotateArrayClockwiseKTimes {
 
     public static void main(String args[]) {
         int array[] = {1, 2, 3, 4, 5};
         int array2[] = {6, 2, 3, 4, 5};
-        int n = array.length;
+
 
         int k = 2;
-        rightRotate(array, k);
+        rotate(array, k);
+        System.out.println(Arrays.toString(array));
 
     }
+    public static int[] rotate(int[] nums,int k)
+    {
+        int n=nums.length;
+        if(k > n)
+            k=k%n;
+        nums=reverse(nums,0,n-1);
+        nums=reverse(nums,0,k-1);
+        nums=reverse(nums,k,n-1);
+        return nums;
+    }
+    public static int[] reverse(int[] nums,int start,int end)
+    {
 
-    // Function to rightRotate array
-    static void rightRotate(int a[], int k) {
-        int n = a.length;
-        // If rotation is greater
-        // than size of array
-        k = k % n;
-
-        for (int i = 0; i < n; i++) {
-            if (i < k) {
-                // Printing rightmost kth elements
-                System.out.print(a[n + i - k]
-                        + " ");
-            } else {
-                // Prints array after
-                // 'k' elements
-                System.out.print(a[i - k]
-                        + " ");
-            }
+        while (start <= end ) {
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
         }
-        System.out.println();
+        return nums;
     }
 
-    static void approach2(int[] array, int k) {
-        int N = array.length;
-        // Reverse all the array elements
-        for (int i = 0; i < N / 2; i++) {
-            int temp = array[i];
-            array[i] = array[N - i - 1];
-            array[N - i - 1] = temp;
-        }
-
-        // Reverse the first k elements
-        for (int i = 0; i < k / 2; i++) {
-            int temp = array[i];
-            array[i] = array[k - i - 1];
-            array[k - i - 1] = temp;
-        }
-
-        // Reverse the elements from K
-        // till the end of the array
-        for (int i = 0; i < (k + N) / 2; i++) {
-            int temp = array[(i + k) % N];
-            array[(i + k) % N] = array[(N - i + k - 1) % N];
-            array[(N - i + k - 1) % N] = temp;
-        }
-
-        // Print the array after rotation
-        for (int i = 0; i < N; i++) {
-
-            System.out.print(array[i] + " ");
-        }
-
-    }
 }
