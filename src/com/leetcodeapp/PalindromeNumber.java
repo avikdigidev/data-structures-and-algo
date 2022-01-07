0056bf861bc076dee8376853c112c94f114b2af1
avikdigidev <avikdigidev@gmail.com>
2022-01-07 22:11:04 +0530

    PalindromeNumber solved

---
+++ src/com/leetcodeapp/PalindromeNumber.java
@@ -0,0 +1,17 @@
+package com.leetcodeapp;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int a = 121;
+        boolean palindromeNumber = isPalindromeNumber(a);
+    }
+
+    private static boolean isPalindromeNumber(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
