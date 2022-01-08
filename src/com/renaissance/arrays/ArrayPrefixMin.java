106511c3378ecf5eb9c85f4ccf280b553c43f7fe
avikdigidev <avikdigidev@gmail.com>
2022-01-08 17:33:13 +0530

    ArrayPrefixMin logic corrected

--- src/com/renaissance/arrays/ArrayPrefixMin.java
+++ src/com/renaissance/arrays/ArrayPrefixMin.java
@@ -2,20 +2,24 @@ package com.renaissance.arrays;
 
 import com.util.MyUtilities;
 
+import java.util.Arrays;
+
 /*Find the prefix min array from a given array starting from 0 till a given index from the array*/
 public class ArrayPrefixMin {
     public static void main(String[] args) {
         int[] inputArray = {4, -85, 15, -125, 42, 41, 14};
         int idx = inputArray.length;
-        int max = getMin(inputArray, idx);
-        System.out.println(max);
+        int[] pMin = getMin(inputArray, idx);
+        System.out.println(Arrays.toString(pMin));
     }
 
-    private static int getMin(int[] inputArray, int idx) {
-        int min = inputArray[0];
-        for (int i = 0; i < idx; i++) {
-            min = MyUtilities.getMin(inputArray[i], min) ;
+    private static int[] getMin(int[] inputArray, int n) {
+        int[] pMin = new int[n];
+        pMin[0] = inputArray[0];
+        for (int i = 1; i < n; i++) {
+            pMin[i] = Math.min(pMin[i - 1], inputArray[i]);
         }
-        return min;
+
+        return pMin;
     }
 }
