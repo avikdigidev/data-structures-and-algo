a9f1b93635d2e820ed49decb1cf49e7da4283787
avikdigidev <avikdigidev@gmail.com>
2022-01-10 23:58:49 +0530

    PowerOfNumber solved

---
+++ src/com/udemy/recursion/PowerOfNumber.java
@@ -0,0 +1,15 @@
+package com.udemy.recursion;
+
+public class PowerOfNumber {
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 3;
+        int result = getPower(base,power);
+        System.out.println(result);
+    }
+
+    private static int getPower(int base, int power) {
+        if (power ==1){return 1;}
+        return base*getPower(base,power-1);
+    }
+}
