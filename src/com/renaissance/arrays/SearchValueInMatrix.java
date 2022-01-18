b3b428c876a7587d9e84b8077e96f544a1632490
avikdigidev <avikdigidev@gmail.com>
2022-01-18 20:36:51 +0530

    SearchValueInMatrix

--- src/com/renaissance/arrays/SearchValueInMatrix.java
+++ src/com/renaissance/arrays/SearchValueInMatrix.java
@@ -1,37 +1,32 @@
 package com.renaissance.arrays;
+
 //https://leetcode.com/problems/search-a-2d-matrix-ii/
 public class SearchValueInMatrix {
     public static void main(String[] args) {
-        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-        int target = 5;
+      //  int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        int target = 3;
+        int[][] matrix ={{-1,3}};
+
         boolean exists = searchMatrix(matrix, target);
         System.out.println(exists);
     }
 
     private static boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length; //no. of columns
-        int n = matrix[0].length; // no. rows
-
+        int m = matrix[0].length; //no of columns
+        int n = matrix.length; //no of rows
         int i = 0;
-        int j = n-1;
-        while (i<n && j>=0){
+        int j = m - 1;
+        while (i < n && j >= 0) {
             int current = matrix[i][j];
-            if (current == target){
-                System.out.println("["+i+","+j+"]");
+            if (current == target) {
+                System.out.println("[" + i + "," + j + "]");
                 return true;
-            }
-            else if (current> target){
+            } else if (current > target) {
                 j--;
-
-            }
-            else{
+            } else {
                 i++;
             }
-
         }
-
-
-
         return false;
     }
 }
