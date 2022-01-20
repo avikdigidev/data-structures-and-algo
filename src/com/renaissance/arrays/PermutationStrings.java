66e28c82880d2e3d8b9d97a542416ac8085bb7c8
avikdigidev <avikdigidev@gmail.com>
2022-01-20 23:55:49 +0530

    PermutationStrings - Initial Commit - NW

---
+++ src/com/renaissance/arrays/PermutationStrings.java
@@ -0,0 +1,13 @@
+package com.renaissance.arrays;
+
+public class PermutationStrings {
+    public static void main(String[] args) {
+        String s1 = "ab", s2 = "eidbaooo";
+     char[] s1arr= s1.toCharArray();
+     char[] s2arr=s2.toCharArray();
+     int count = 0;
+        for (int i = 0; i < s2arr.length; i++) {
+            if (s1.(s2arr[i])) count++;
+        }
+    }
+}
