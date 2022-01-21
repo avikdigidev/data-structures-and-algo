288d2bb42baf20f66aaaf7a59eab4a38e256ba4c
avikdigidev <avikdigidev@gmail.com>
2022-01-21 23:53:13 +0530

    SumOfThree -brute force approach solved

--- src/com/leetcodeapp/easy/SumOfThree.java
+++ src/com/leetcodeapp/easy/SumOfThree.java
@@ -8,47 +8,25 @@ import java.util.Arrays;
 public class SumOfThree {
     public static void main(String[] args) {
         int[] arr = {-1, 0, 1, 2, -1, -4};
-        int[][] result = sumOfThree(arr);
-        System.out.println(Arrays.toString(result));
+        naiveSumOfThree(arr);
     }
 
-    private static int[][] sumOfThree(int[] arr) {
-        if (arr.length == 0) return new int[0][0];
+    private static void naiveSumOfThree(int[] arr) {
+        if (arr.length == 0) return;
         int n = arr.length;
-        int[][] result = new int[n][n];
-
-        //sort the array
-
-        for (int i = 0; i < n-2; i++) {
-            if (arr[i] > 0){ //sum cannot be 0 since arr[i] is 0 already
-                break;
-            }
-            if (i>0 && arr[i] == arr[i-1]){ //skip duplicates
-                continue;
-            }
-            //similar to Two sum we are to find ints that sum to a target
-            int j = i+1; //starts after i
-            int k = n-1; //ends at the end of the array
-            while (j<k){
-                if (arr[i] +arr[j]>0){break;}
-                if(j> i+1&&arr[j] == arr[j-1]){ //skip duplicates
-                    j++;
-                    continue;
-                }
-                if (arr[i] +arr[j]+ arr[k] >0){k--;}
-                else if (arr[i] +arr[j]+ arr[k] <0){j++;}
-
-                else{
-                    int[] res = {arr[i], arr[j], arr[k]};
-                    result[i] = res;
-                    j++;k--;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        System.out.println(arr[i] + "," + arr[j] + "," + arr[k]);
+                        break;
+                    } else {
+                        System.out.println("no triplet");
+                    }
                 }
             }
+
         }
-
-
-        return result;
-
     }
-
 }
+
