05b208d6e1de8f7a099ce7ea1ec9b179612f2b02
avikdigidev <avikdigidev@gmail.com>
2022-01-26 23:16:17 +0530

    ListOfPrimes solved - O(N) approach

--- src/com/renaissance/maths/ListOfPrimes.java
+++ src/com/renaissance/maths/ListOfPrimes.java
@@ -3,7 +3,19 @@ package com.renaissance.maths;
 public class ListOfPrimes {
     public static void main(String[] args) {
         int n = 100;
-        getBruteForcePrimes(n);
+        getBruteForcePrimes(n); //O(N*Math.sqrt(N))
+        System.out.println();
+        getPrimeEfficient(n); //O(N)
+
+    }
+
+    private static void getPrimeEfficient(int n) {
+        System.out.print(2+","+3+",");
+        for (int i = 1; (6*i + 1) <=n ; i++) {
+
+            System.out.print((6*i - 1) +","+(6*i + 1)+",");
+
+        }
     }
 
     private static void getBruteForcePrimes(int n) {
