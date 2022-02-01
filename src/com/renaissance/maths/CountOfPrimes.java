825bdc7f711fc9e5f29584fe1ca492621637177a
avikdigidev <avikdigidev@gmail.com>
2022-02-01 22:53:23 +0530

    CountOfPrimes solved

---
+++ src/com/renaissance/maths/CountOfPrimes.java
@@ -0,0 +1,37 @@
+package com.renaissance.maths;
+
+import java.util.Scanner;
+
+public class CountOfPrimes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res = getCountOfPrimes(n);
+        System.out.println(res);
+    }
+
+    private static int getCountOfPrimes(int n) {
+        int count = 0;
+        boolean[] primes = new boolean[n + 1];
+        //creating boolean array of n+1 size and initialize with all true
+        for (int i = 1; i < primes.length; i++) {
+            primes[i] = true;
+        }
+        // 1 is not prime
+        primes[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (primes[i]) {
+                for (int j = i; i * j <= n; j++) {
+                    primes[i * j] = false;
+                }
+            }
+        }
+        //getting only prime
+        for (int i = 2; i < primes.length; i++) {
+            if (i < n && primes[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
