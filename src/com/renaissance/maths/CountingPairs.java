413489d0e9a23abcca6bd9d7c7e45625cda0c1cf
avikdigidev <avikdigidev@gmail.com>
2022-02-09 23:57:20 +0530

    CountingPairs Solved

--- src/com/renaissance/maths/CountingPairs.java
+++ src/com/renaissance/maths/CountingPairs.java
@@ -10,7 +10,8 @@ nums[i] + nums[j] % k ==0
 * */
 public class CountingPairs {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 6, 456, 568, 4, 65, 54, 75, 8};
+    //    int[] arr = {2,2,1,7,5,3};
+        int[] arr = {1,3,6,456,568,4,65,54,75,8};
         int k = 3;
         int result = getCountingPairsEfficiently(arr, k);
         System.out.println(result);
@@ -26,13 +27,14 @@ public class CountingPairs {
         for (int i = 0; i < k; i++) {
             countArray[i] = 0; //initializing the count to 0
         }
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
+            int a = arr[i];
             countArray[arr[i] % k]++; //increasing the count value at the index whenever we get the corresponding value
         }
         //considering those pairs for which arr[i]%k==0
-        ans += (countArray[0] + countArray[0] - 1) / 2;
+        ans += (countArray[0] * (countArray[0] - 1)) / 2;
         //taking care of other buckets
-        for (int i = 1; i <= (k / 2) - 1; i++) {
+        for (int i = 1; i <= k / 2 - 1; i++) {
             ans += countArray[i] * countArray[k - i];
         }
         //taking care of those pairs when k is either odd or even
@@ -40,7 +42,7 @@ public class CountingPairs {
         if (k % 2 == 0) {
             ans += (countArray[k / 2] * (countArray[k / 2] - 1)) / 2;
         } else {
-            ans += countArray[k / 2] * countArray[(k / 2) + 1];
+            ans += countArray[k / 2] * countArray[k / 2+ 1];
         }
         return ans;
     }
