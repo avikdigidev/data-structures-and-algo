fd9cd5b8df38455feecc8c98870536d511ae0e76
avikdigidev <avikdigidev@gmail.com>
2022-02-11 00:09:04 +0530

    CountingTriplets brute force runtime efficiency added

--- src/com/renaissance/maths/CountingTriplets.java
+++ src/com/renaissance/maths/CountingTriplets.java
@@ -2,9 +2,9 @@ package com.renaissance.maths;
 
 public class CountingTriplets {
     public static void main(String[] args) {
-int[] arr = {1,2,5};
-int m = 8;
-int count = getCountOfTripletsBruteForce(arr,m);
+        int[] arr = {1, 2, 5};
+        int m = 8;
+        int count = getCountOfTripletsBruteForce(arr, m); //O(N^3)
         System.out.println(count);
     }
 
@@ -12,9 +12,9 @@ int count = getCountOfTripletsBruteForce(arr,m);
         int count = 0;
         int n = arr.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                for (int k = j+1; k < n; k++) {
-                    if ((arr[i]+arr[j]+arr[k])%m==0){
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if ((arr[i] + arr[j] + arr[k]) % m == 0) {
                         count++;
                     }
                 }
