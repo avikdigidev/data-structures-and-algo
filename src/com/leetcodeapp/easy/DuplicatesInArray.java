a28d4c0107bb85c135a6e7579c463a709373672b
avikdigidev <avikdigidev@gmail.com>
2022-02-14 22:41:56 +0530

    DuplicatesInArray

--- src/com/leetcodeapp/easy/DuplicatesInArray.java
+++ src/com/leetcodeapp/easy/DuplicatesInArray.java
@@ -2,7 +2,7 @@ package com.leetcodeapp.easy;
 
 import java.util.HashSet;
 import java.util.Set;
-
+//https://leetcode.com/problems/contains-duplicate/
 public class DuplicatesInArray {
     public static void main(String[] args) {
         int[] arr = {1,5,22,3,1,2,31,65,9};
@@ -12,7 +12,7 @@ public class DuplicatesInArray {
 
 
     private static boolean findDuplicate(int[] arr) {
-        Set<Integer> intSet = new HashSet<Integer>(arr.length);
+        Set<Integer> intSet = new HashSet<>(arr.length);
         for(int x : arr){
             if (intSet.contains(x)) return true;
             intSet.add(x);
