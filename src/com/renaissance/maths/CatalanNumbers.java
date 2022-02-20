91063292863d0f21d09e60ff8935c8880478536b
avikdigidev <avikdigidev@gmail.com>
2022-02-20 23:37:31 +0530

    CatalanNumbers solved

---
+++ src/com/renaissance/maths/CatalanNumbers.java
@@ -0,0 +1,24 @@
+package com.renaissance.maths;
+
+public class CatalanNumbers {
+    public static void main(String[] args) {
+        //total n pairs of balanced paranthesis
+        int n = 5;
+        long count = getCatalanNumbers(n);
+        System.out.println(count);
+    }
+
+    private static long getCatalanNumbers(int n) {
+        long[] countArray = new long[n + 1];
+        countArray[0] = 1; //no pair of bracket is also considered balanced
+        countArray[1] = 1; // 1 pair will be balanced only ()
+        for (int i = 2; i <= n; i++) {
+            countArray[i] = 0;
+            for (int j = 0; j <= i - 1; j++) {
+                countArray[i] += countArray[j] * countArray[i - j - 1];
+            }
+
+        }
+        return countArray[n];
+    }
+}
