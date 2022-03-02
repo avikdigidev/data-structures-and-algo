6ae83cbed30970f702bf0dc46333ec562a4f63a0
avikdigidev <avikdigidev@gmail.com>
2022-03-02 23:35:55 +0530

    HeightChecker efficiently solved

--- src/com/leetcode/website/arrays101/HeightChecker.java
+++ src/com/leetcode/website/arrays101/HeightChecker.java
@@ -10,15 +10,43 @@ import java.util.Arrays;
  * You are given an integer array heights representing the current order that the students are standing in.
  * Each heights[i] is the height of the ith student in line (0-indexed).
  * Return the number of indices where: heights[i] != expected[i].
+ * Constraints:
+ * 1 <= heights.length <= 100
+ * 1 <= heights[i] <= 100
  */
 public class HeightChecker {
     public static void main(String[] args) {
         int[] arr = {1, 1, 4, 2, 1, 3};
-        System.out.println(checkheight(arr));
+        System.out.println(checkHeightUsingExtraSpace(arr));
+        System.out.println(heightChecker(arr));
 
     }
 
-    private static int checkheight(int[] arr) {
+    private static int heightChecker(int[] heights) {
+        int[] heightToFreq = new int[101]; // heights go from 1 to 100
+
+        for (int height : heights) {
+            heightToFreq[height]++;
+        }
+
+        int result = 0;
+        int curHeight = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            while (heightToFreq[curHeight] == 0) {
+                curHeight++;
+            }
+
+            if (curHeight != heights[i]) {
+                result++;
+            }
+            heightToFreq[curHeight]--;
+        }
+
+        return result;
+    }
+
+    private static int checkHeightUsingExtraSpace(int[] arr) {
         int[] temp = new int[arr.length];
         int j = 0;
         for (int a : arr) {
