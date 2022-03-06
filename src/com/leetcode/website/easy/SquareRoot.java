eb11dd3b082608fd9f387bf953fbf1e6006b6888
avikdigidev <avikdigidev@gmail.com>
2022-03-07 03:17:36 +0530

    SquareRoot solved with Newton Rapson Approach

--- src/com/leetcode/website/easy/SquareRoot.java
+++ src/com/leetcode/website/easy/SquareRoot.java
@@ -1,10 +1,56 @@
 package com.leetcode.website.easy;
+
 //https://leetcode.com/problems/sqrtx/
 public class SquareRoot {
     public static void main(String[] args) {
-        int num = 8;
-        int root = getSquareRootRepeatedSubtractionMethod(num);
+        int num = 40;
+        int precision = 3;
+     //   getBinarySearchSquareRoot(num, precision); //T.C:   O(Log(n))
+       //getSquareRootRepeatedSubtractionMethod(num);
+       getSquareRootNewtonRaphsonApproach(num);
+
+
+    }
+
+    private static double getSquareRootNewtonRaphsonApproach(int num) {
+        double x = num;
+        double root =0.0;
+        while(true){
+            root = 0.5*(x+(num/x));
+            if (Math.abs(root-x)<0.55){
+                break;
+            }
+            x=root;
+        }
         System.out.println(root);
+        return root;
+    }
+
+    private static double getBinarySearchSquareRoot(int num, double precision) {
+        int start = 0;
+        int end = num;
+        double root = 0.000;
+        while (start <= end) {
+            int midPoint = start + (end - start) / 2;
+            if (midPoint*midPoint==num){
+                return new Double(midPoint);
+            }
+            if (midPoint*midPoint>num){
+                end = midPoint-1;
+            }else{
+                start = midPoint+1;
+            }
+        }
+        double increment = 0.1;
+        for (int i = 0; i < precision; i++) {
+            while (root*root<=num){
+                root+=increment;
+            }
+            root-=increment;
+            increment/=10;
+        }
+        System.out.println(root);
+        return root;
     }
 
     private static int getSquareRootRepeatedSubtractionMethod(int num) {
@@ -18,6 +64,7 @@ public class SquareRoot {
                 return result;
             }
         }
+        System.out.println(result);
         return result;
     }
 }
