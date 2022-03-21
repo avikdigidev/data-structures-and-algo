e9722817396dcd23ced35754bab177807281e4a0
avikdigidev <avikdigidev@gmail.com>
2022-03-22 00:30:15 +0530

    MergeTwoSortedArrays revised

--- src/com/leetcode/website/arrays101/MergeTwoSortedArrays.java
+++ src/com/leetcode/website/arrays101/MergeTwoSortedArrays.java
@@ -19,19 +19,19 @@ import java.util.Arrays;
 
 public class MergeTwoSortedArrays {
     public static void main(String[] args) {
-        int[] a = {1,2,3,0,0,0};
+        int[] a = {1, 2, 6, 0, 0, 0};
         int[] b = {4, 5, 51};
         int m = 3;
         int n = 3;
-        merge(a, m, b, n);
+        merge(a, b, m, n);
         System.out.println(Arrays.toString(a));
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1,j=n-1;
-        int idx = m+n-1;
-        while(j>=0) {
-            if(i>=0 && nums1[i] > nums2[j]) {
+    public static void merge(int[] nums1, int[] nums2, int m, int n) {
+        int i = m - 1, j = n - 1;
+        int idx = m + n - 1;
+        while (j >= 0 && i >= 0) {
+            if (nums1[i] > nums2[j]) {
                 nums1[idx] = nums1[i--];
             } else {
                 nums1[idx] = nums2[j--];
