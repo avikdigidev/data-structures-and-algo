967f3a1f24170b166400453c720ef3ead7ab54b3
avikdigidev <avikdigidev@gmail.com>
2022-03-28 18:00:53 +0530

    interview test 1/3 solved

--- src/com/interview/test/Accolite.java
+++ src/com/interview/test/Accolite.java
@@ -1,6 +1,7 @@
 package com.interview.test;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class Accolite {
@@ -9,15 +10,15 @@ public class Accolite {
         hm.put(1, "Prakash");
         hm.put(2, "Pradeep");
         hm.put(3, "Alas");
-        hm.put(4, "aPrakash");
-        hm.put(4, "Pash");
+        hm.put(4, "APrakash");
+        hm.put(5, "Pash");
         int[] arr = {9, 8, 7, 6, 1, 2, 3, 4, 5, 2};
 
                 // TODO sort NlogN
         // TODO without doing any iteration add, delete and min in O(1) maybe by stack
         // TODO sort hm based on values using Stream.
 
-
+hm.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(s-> System.out.println(s));
 
     }
 
