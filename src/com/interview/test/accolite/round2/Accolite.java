b54b5ab7919562bbb7aa2e4ef1fab843db127812
avikdigidev <avikdigidev@gmail.com>
2022-04-04 23:32:18 +0530

    Accolite interview round2 2/3 solved

--- src/com/interview/test/accolite/round2/Accolite.java
+++ src/com/interview/test/accolite/round2/Accolite.java
@@ -1,25 +1,64 @@
 package com.interview.test.accolite.round2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
 public class Accolite {
     public static void main(String[] args) {
+        // sort array with O(NlogN) efficiency
+        int[] array = {9, 8, 7, 6, 1, 2, 3, 4, 5, 2};
+        mergeSort(array, array.length);
+        System.out.println(Arrays.toString(array));
+        // sort hashMap based on values using Stream.
         HashMap<Integer, String> hm = new HashMap<>();
         hm.put(1, "Prakash");
         hm.put(2, "Pradeep");
         hm.put(3, "Alas");
         hm.put(4, "aPrakash");
         hm.put(5, "Pash");
-        int[] arr = {9, 8, 7, 6, 1, 2, 3, 4, 5, 2};
-
-                // TODO sort NlogN
+        hm.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(e -> System.out.println(e.getValue()));
 
         // TODO without doing any iteration add, delete and min in O(1) maybe by stack
-         // sort hm based on values using Stream.
-hm.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(e-> System.out.println(e.getValue()));
 
+    }
+
+    private static void mergeSort(int[] array, int length) {
+        if (length < 2) return;
+        int mid = length / 2;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length - mid];
+        int rightArrayIndex = 0;
+        for (int leftArrayIndex = 0; leftArrayIndex < length; leftArrayIndex++) {
+            if (leftArrayIndex < mid) {
+                leftArray[leftArrayIndex] = array[leftArrayIndex];
+            } else {
+                rightArray[rightArrayIndex] = array[leftArrayIndex];
+                rightArrayIndex++;
+            }
+        }
+        mergeSort(leftArray, mid);
+        mergeSort(rightArray, length - mid);
+
+        merge(leftArray, rightArray, array, mid, length - mid);
+    }
+
+    private static void merge(int[] leftArray, int[] rightArray, int[] array, int leftSize, int rightSize) {
+        int i = 0, leftIndex = 0, rightIndex = 0;
+        while (leftIndex < leftSize && rightIndex < rightSize) {
+            if (leftArray[leftIndex] < rightArray[rightIndex]) {
+                array[i++] = leftArray[leftIndex++];
+            }else{
+                array[i++]= rightArray[rightIndex++];
+            }
+        }
+        while (leftIndex < leftSize){
+            array[i++] = leftArray[leftIndex++];
+        }
+        while (rightIndex < rightSize){
+            array[i++]= rightArray[rightIndex++];
+        }
 
     }
 
