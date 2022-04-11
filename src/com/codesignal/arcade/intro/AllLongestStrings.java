b371c70be1be1a4a3d710ebf49acc0157efcdde0
avikdigidev <avikdigidev@gmail.com>
2022-04-11 23:43:00 +0530

    AllLongestStrings solved using java 8 streams

--- src/com/codesignal/arcade/intro/AllLongestStrings.java
+++ src/com/codesignal/arcade/intro/AllLongestStrings.java
@@ -1,6 +1,8 @@
 package com.codesignal.arcade.intro;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
+
 /*
 * Given an array of strings, return another array containing all of its longest strings.
 
@@ -28,31 +30,44 @@ public class AllLongestStrings {
     public static void main(String[] args) {
         String[] input= {"awsgf","af","adsya", "asju", "bbd"};
         System.out.println(Arrays.toString(solution(input)));
+        System.out.println(Arrays.toString(solution2(input)));
     }
    static String[] solution(String[] inputArray) {
         int max =Integer.MIN_VALUE;
         int count = 0;
 
-        for(int i =0;i<inputArray.length;i++){
-            max = Math.max(inputArray[i].length(), max);
-        }
-        for(int i =0;i<inputArray.length;i++){
-            if(inputArray[i].length()==max){
-                count++;
-            }
+       for (String s : inputArray) {
+           max = Math.max(s.length(), max);
+       }
+       for (String s : inputArray) {
+           if (s.length() == max) {
+               count++;
+           }
 
-        }
+       }
         String[] result = new String[count];
         int k =0;
-        for(int i =0;i<inputArray.length;i++){
-            if(inputArray[i].length()==max){
-                result[k]=inputArray[i];
-                k++;
-            }
+       for (String s : inputArray) {
+           if (s.length() == max) {
+               result[k] = s;
+               k++;
+           }
 
+       }
+        return result;
+    }
+
+    static String[] solution2(String[] inputArray) {
+        int max =Integer.MIN_VALUE;
+        for (String value : inputArray) {
+            max = Math.max(value.length(), max);
         }
 
-        return result;
+        int finalMax = max;
+        return Stream.of(inputArray)
+                .filter(s -> s.length() == finalMax)
+                .toArray(String[]::new);
+
     }
 
 }
