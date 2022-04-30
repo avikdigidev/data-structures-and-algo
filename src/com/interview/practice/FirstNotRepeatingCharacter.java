7a5aa123e256e7907078382d827e858236e7bd42
avikdigidev <avikdigidev@gmail.com>
2022-04-30 23:46:52 +0530

    firstNotRepeatingCharacter WIP

---
+++ src/com/interview/practice/FirstNotRepeatingCharacter.java
@@ -0,0 +1,15 @@
+package com.interview.practice;
+
+public class FirstNotRepeatingCharacter {
+    public static void main(String[] args) {
+        String s = "abacabad";
+        char result = getFirstNotRepeatingCharacter(s);
+    }
+
+    private static char getFirstNotRepeatingCharacter(String s) {
+        char result = 0;
+
+      //TODO implement this
+        return result;
+    }
+}
