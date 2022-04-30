9c778654cd7e4ee1bb33a26634d53f2380df0926
avikdigidev <avikdigidev@gmail.com>
2022-05-01 03:25:49 +0530

    firstNotRepeatingCharacter solved using 2 approaches

--- src/com/interview/practice/FirstNotRepeatingCharacter.java
+++ src/com/interview/practice/FirstNotRepeatingCharacter.java
@@ -3,13 +3,32 @@ package com.interview.practice;
 public class FirstNotRepeatingCharacter {
     public static void main(String[] args) {
         String s = "abacabad";
-        char result = getFirstNotRepeatingCharacter(s);
+        char result = getFirstNotRepeatingCharacterUsingStringInBuilt(s);
+        char result2 = getFirstNotRepeatingCharacter(s);
+        System.out.println(result);
+        System.out.println(result2);
     }
 
     private static char getFirstNotRepeatingCharacter(String s) {
-        char result = 0;
+        int[] chars = new int[256];
+        for (char c : s.toCharArray()) {
+            chars[c]++;
 
-      //TODO implement this
-        return result;
+        }
+        for (char c : s.toCharArray()) {
+            if (chars[c] ==1) return c;
+
+        }
+        return '_';
+    }
+
+    private static char getFirstNotRepeatingCharacterUsingStringInBuilt(String s) {
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.lastIndexOf(s.charAt(i)) == s.indexOf(s.charAt(i)))
+                return s.charAt(i);
+        }
+        return '_';
+
     }
 }
