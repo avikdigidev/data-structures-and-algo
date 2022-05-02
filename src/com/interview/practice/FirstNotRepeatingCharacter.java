c2f54b41b7eabe572f8366b278202ff36e645e17
avikdigidev <avikdigidev@gmail.com>
2022-05-02 23:04:27 +0530

    firstNotRepeatingCharacter solved most efficient approach

--- src/com/interview/practice/FirstNotRepeatingCharacter.java
+++ src/com/interview/practice/FirstNotRepeatingCharacter.java
@@ -1,12 +1,39 @@
 package com.interview.practice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstNotRepeatingCharacter {
     public static void main(String[] args) {
         String s = "abacabad";
         char result = getFirstNotRepeatingCharacterUsingStringInBuilt(s);
         char result2 = getFirstNotRepeatingCharacter(s);
+        char result3 = getFirstNotRepeatingCharacterMostEfficient(s);
         System.out.println(result);
         System.out.println(result2);
+        System.out.println(result3);
+    }
+
+    private static char getFirstNotRepeatingCharacterMostEfficient(String s) {
+        int noOfChars = 256;
+        HashMap<Character, CountIndex> hm = new HashMap<>(noOfChars);
+        for (int i = 0; i < s.length(); i++) {
+            if (hm.containsKey(s.charAt(i))) {
+                hm.get(s.charAt(i)).incCount();
+            } else {
+                hm.put(s.charAt(i), new CountIndex(i));
+            }
+        }
+
+        int result = Integer.MAX_VALUE, i;
+        for (Map.Entry<Character, CountIndex> entry : hm.entrySet()) {
+            int c = entry.getValue().count;
+            int ind = entry.getValue().index;
+            if (c == 1 && ind < result) {
+                result = ind;
+            }
+        }
+        return s.charAt(result);
     }
 
     private static char getFirstNotRepeatingCharacter(String s) {
@@ -16,7 +43,7 @@ public class FirstNotRepeatingCharacter {
 
         }
         for (char c : s.toCharArray()) {
-            if (chars[c] ==1) return c;
+            if (chars[c] == 1) return c;
 
         }
         return '_';
@@ -30,5 +57,18 @@ public class FirstNotRepeatingCharacter {
         }
         return '_';
 
+    }
+}
+
+class CountIndex {
+    int count, index;
+
+    public CountIndex(int index) {
+        this.count = 1;
+        this.index = index;
+    }
+
+    public void incCount() {
+        this.count++;
     }
 }
