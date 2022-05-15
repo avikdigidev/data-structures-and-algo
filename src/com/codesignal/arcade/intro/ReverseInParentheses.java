d2247917471e4daa4ac7c4d2341c4f534c3e8a5f
avikdigidev <avikdigidev@gmail.com>
2022-05-15 22:38:29 +0530

    ReverseInParentheses solved

--- src/com/codesignal/arcade/intro/ReverseInParentheses.java
+++ src/com/codesignal/arcade/intro/ReverseInParentheses.java
@@ -1,24 +1,55 @@
 package com.codesignal.arcade.intro;
 
+import java.util.Stack;
+
+
 public class ReverseInParentheses {
     public static void main(String[] args) {
-        String s = reverseInsideParanthesis("foo(bar)baz(blim)"); //"foorabblim"
+        String s = reverseInsideParenthesis("foo(bar)baz(blim)"); //"foorabblim"
 
         System.out.println(s);
     }
 
-    private static String reverseInsideParanthesis(String s) {
-        int firstInd = s.lastIndexOf('(');
-        int lastInd = s.indexOf(')', firstInd);
-        while (firstInd != -1) {
-            String revStr = new StringBuilder(s.substring(firstInd + 1, lastInd)).reverse().toString();
-            String first = s.substring(0, firstInd);
-            String last = s.substring(lastInd + 1);
-            s = first + revStr + last;
-            firstInd = s.lastIndexOf('(');
-            lastInd = s.indexOf('(', firstInd);
+    static void reverse(char A[], int l, int h) {
+        if (l < h) {
+            char ch = A[l];
+            A[l] = A[h];
+            A[h] = ch;
+            reverse(A, l + 1, h - 1);
         }
-        return s;
+    }
+
+    private static String reverseInsideParenthesis(String s) {
+
+        Stack<Integer> st = new Stack<Integer>();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+
+            // Push the index of the current
+            // opening bracket
+            if (s.charAt(i) == '(') {
+                st.push(i);
+            }
+
+            // Reverse the substring starting
+            // after the last encountered opening
+            // bracket till the current character
+            else if (s.charAt(i) == ')') {
+                char[] A = s.toCharArray();
+                reverse(A, st.peek() + 1, i);
+                s = String.copyValueOf(A);
+                st.pop();
+            }
+        }
+
+        // To store the modified string
+        String res = "";
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) != ')' && s.charAt(i) != '(') {
+                res += (s.charAt(i));
+            }
+        }
+        return res;
     }
 }
 
