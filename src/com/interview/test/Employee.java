04c64a9bdac37315379b5a6c081c23f13e195325
avikdigidev <avikdigidev@gmail.com>
2022-05-17 18:07:11 +0530

    Tavant interview

---
+++ src/com/interview/test/Employee.java
@@ -0,0 +1,6 @@
+package com.interview.test;
+
+public class Employee {
+private int empId;
+private String
+}
---
+++ src/com/interview/test/Tavent.java
@@ -0,0 +1,21 @@
+package com.interview.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tavent {
+    public static void main(String[] args) {
+/*
+        List<String> ls = new ArrayList<>();
+        ls.parallelStream().filter().collect(Collectors.toList());
+
+
+        List<Employee> employeeList = new ArrayList<Employee>();
+
+        employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+        employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
+        employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
+employeeList.stream().map(e1 -> e1.getDepartment()).sorted(e1, e2 -> e1.getSalary().compareTo(e2.getSalary())).max();
+*/    }
+
+}
