fb9d8a04e31a5026bdb81414d9e8750930c412b7
avikdigidev <avikdigidev@gmail.com>
2022-10-11 01:54:08 +0530

    DivideAndConquer solved

--- src/com/renaissance/recursion/DivideAndConquer.java
+++ src/com/renaissance/recursion/DivideAndConquer.java
@@ -6,6 +6,15 @@ public class DivideAndConquer {
         int start = 0;
         int end = arr.length-1;
         int max = getMax(arr, start, end);
+        //T.C > N + N/2 + N/4 + N/8 ....1
+        /*
+        = N(1+1/2+1/4+1/8+.....1)
+        = N(a/1-r)          // where a = 1 and r= 1/2
+        As we know that a/1-r ~ 2
+        then T.C. will be less than 2N
+      */
+
+        // S.C> log2 (N)
         System.out.println(max);
     }
 
