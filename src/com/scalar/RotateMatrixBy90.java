25afa399cba6c795eb0c5d51dedb842ef3d7e952
prakash shelke <avikdigidev@gmail.com>
2024-03-18 00:26:43 +0530

    RotateMatrixBy90 solved

---
+++ src/com/scalar/RotateMatrixBy90.java
@@ -0,0 +1,44 @@
+package com.scalar;
+
+import java.util.Arrays;
+
+public class RotateMatrixBy90 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 77}, {4, 5, 6, 88}, {7, 8, 9, 99}, {100, 101, 1023, 15}};
+        System.out.println(Arrays.toString(matrix[0]));
+        System.out.println(Arrays.toString(matrix[1]));
+        System.out.println(Arrays.toString(matrix[2]));
+        System.out.println(Arrays.toString(matrix[3]));
+        System.out.println("************Rotated Matrix*****************");
+        rotate(matrix);
+        System.out.println(Arrays.toString(matrix[0]));
+        System.out.println(Arrays.toString(matrix[1]));
+        System.out.println(Arrays.toString(matrix[2]));
+        System.out.println(Arrays.toString(matrix[3]));
+    }
+
+    private static void rotate(int[][] matrix) {
+        int n = matrix[0].length;
+        //transpose of matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //swap rows
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
