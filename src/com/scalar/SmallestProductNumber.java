abfdda1a5553461de18f0d0c87d814f6bec2dd1e
prakash shelke <avikdigidev@gmail.com>
2024-03-17 23:07:37 +0530

    SmallestProductNumber solved well

--- src/com/scalar/SmallestProductNumber.java
+++ src/com/scalar/SmallestProductNumber.java
@@ -1,39 +1,21 @@
 package com.scalar;
 
-import java.util.HashMap;
-
 public class SmallestProductNumber {
     public static void main(String[] args) {
-        int result = smallestProductNumber(1000);
+        int result = smallestProductNumber(100);
         System.out.println(result);
     }
 
     private static int smallestProductNumber(int n) {
-        HashMap<Integer, Integer> factorCount = new HashMap<>(10);
-        for (int i = 0; i < 10; i++) {
-            factorCount.put(i, 0);
-        }
-        int fact = 9;
-        while (n > 1) {
-            if (n % fact == 0) {
+        String s = "";
+
+        for (int fact = 9; fact >1; fact--) {
+            while (n % fact == 0) {
                 n = n / fact;
-                factorCount.put(fact, factorCount.get(fact) + 1);
-            }
-            if (n % fact != 0) {
-                fact--;
+                s = fact + s;
             }
         }
-
-        StringBuilder s = new StringBuilder("");
-        for (int i = 0; i < factorCount.size(); i++) {
-            if (factorCount.get(i) != 0) {
-                Integer times = factorCount.get(i);
-                for (int j = 0; j < times; j++) {
-                    s.append(i);
-                }
-            }
-
-        }
-        return Integer.parseInt(s.toString());
+        if (n != 1) return -1;
+        return Integer.parseInt(s);
     }
 }
